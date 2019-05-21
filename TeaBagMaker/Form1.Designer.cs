28d648e7d8e83a42c3ced05c20f2ea3918c07e3c
Hyoeun Jung <hyodaeun@naver.com>
2019-05-21 11:26:46 +0900

    초기 커밋.

---
+++ TeaBagMaker/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace TeaBagMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbList = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbList
+            // 
+            this.cbList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbList.FormattingEnabled = true;
+            this.cbList.Location = new System.Drawing.Point(59, 12);
+            this.cbList.Name = "cbList";
+            this.cbList.Size = new System.Drawing.Size(121, 20);
+            this.cbList.TabIndex = 0;
+            this.cbList.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "담그기!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "2019-05-21, 3615 정효은";
+            this.label1.UseWaitCursor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(87, 52);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(69, 12);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "남은 시간 : ";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 148);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "차 종류를 선택하세요";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ TeaBagMaker/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.VisualBasic;
+
+namespace TeaBagMaker
+{
+    public partial class Form1 : Form
+    {
+        string[] teaList = new string[]{"홍차", "녹차", "루이보스차", "국화차"};
+        string orgStr = "";
+        int CountOrgNum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < teaList.Length; i++)
+            {
+                this.cbList.Items.Add(teaList[i]);
+            }
+            if (cbList.Items.Count > 0)
+            {
+                this.cbList.SelectedIndex = 0;
+            }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (this.cbList.Text == "홍차")
+            {
+                this.lblResult.Text = "2분";
+            }
+            if (this.cbList.Text == "녹차")
+            {
+                this.lblResult.Text = "3분";
+            }
+            if (this.cbList.Text == "루이보스차")
+            {
+                this.lblResult.Text = "5분";
+            }
+            if (this.cbList.Text == "국화차")
+            {
+                this.lblResult.Text = "2분";
+            }
+
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if(this.cbList.Text == "홍차")
+            {
+                this.Timer.Enabled = false;
+                this.lblResult.Text = orgStr;
+
+            }
+            if (this.cbList.Text == "녹차")
+            {
+                this.Timer.Enabled = false;
+                this.lblResult.Text = orgStr;
+
+            }
+            if (this.cbList.Text == "루이보스차")
+            {
+                this.Timer.Enabled = false;
+                this.lblResult.Text = orgStr;
+
+            }
+            if (this.cbList.Text == "국화차")
+            {
+                this.Timer.Enabled = false;
+                this.lblResult.Text = orgStr;
+
+            }
+        }
+
+        private bool IntCheck()
+        {
+            if (Information.IsNumeric(this.lblResult.Text))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("숫자를 입력하세요~", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+    }
+}
